440defcd7b6c046d8886453aaf2dd3a104f52cb6
luk1ta <lucatms0@gmail.com>
2022-01-24 22:57:55 -0300

    modificaciones parametros
    -moveplayer
    -moveenemy
    -addlife player
    -damagelife player

--- Proyecto Final/Assets/Script/MoveEnemy/MoveEnemy.cs
+++ Proyecto Final/Assets/Script/MoveEnemy/MoveEnemy.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MoveEnemy : MonoBehaviour
 {
+    public float Speedenemy = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(16,2,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      EnemyMove(Vector3.forward);
+    }
+    void EnemyMove( Vector3 direccion){
+        transform.Translate(Speedenemy * Time.deltaTime * direccion);
     }
 }
--- Proyecto Final/Assets/Script/MovePlayer/Moveplayer.cs
+++ Proyecto Final/Assets/Script/MovePlayer/Moveplayer.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Moveplayer : MonoBehaviour
 {
     public int lifeplayer = 100;
-    public float Speedplayer = 1.5f;
+    public float Speedplayer = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(30,2,1);
+        transform.position = new Vector3(-10,2,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer(Vector3.back);
+    }
+    void MovePlayer(Vector3 direccion){
+    transform.Translate(Speedplayer * Time.deltaTime * direccion);
+    }
+    void damage( int dmg){
+        lifeplayer -= dmg;
+    }
+    void addlife(int addlife){
+        lifeplayer += addlife;
     }
 }
